f7f48cc754594197463063efde1c337ad0301d2d
jsanderswp <jsanders3141@icloud.com>
2026-04-21 09:21:56 -0700

    created api database, established endpoints to get planet by name and list of all planets

--- StarTrekWeatherAPI/Models/Planet.cs
+++ StarTrekWeatherAPI/Models/Planet.cs
@@ -1,10 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+[Table("planets")]
 public class Planet
 {
+    [Key]
+    [Column("name")]
     public string Name { get; set; } = "";
+
+    [Column("solar_system")]
     public string SolarSystem { get; set; } = "";
-    public string MeanTemp { get; set; } = "";
-    public string AtmosphericPressure { get; set; } = "";
+
+    [Column("atmospheric_pressure")]
+    public float AtmosphericPressure { get; set; }
+
+    [Column("max_temp")]
     public float MaxTemp { get; set; }
+
+    [Column("min_temp")]
     public float MinTemp { get; set; }
+
+    [Column("description")]
     public string Description { get; set; } = "";
 }
--- StarTrekWeatherAPI/Program.cs
+++ StarTrekWeatherAPI/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
@@ -12,6 +14,9 @@ builder.Services.AddCors(options =>
             .AllowAnyHeader();
     });
 });
+
+builder.Services.AddDbContext<ApiDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
