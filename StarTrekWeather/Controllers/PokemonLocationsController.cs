d11c96100f5c398c463d166bd0e70f0660c98c52
jsanderswp <jsanders3141@icloud.com>
2026-05-07 22:55:06 -0700

    added loactions endpoint to PokemonLocationsController.cs

--- StarTrekWeather/Controllers/PokemonLocationsController.cs
+++ StarTrekWeather/Controllers/PokemonLocationsController.cs
@@ -9,6 +9,12 @@ public class PokemonLocationsController
         _client = httpClientFactory.CreateClient("ExternalApi");
     }
     
-    
+    [HttpGet("locations")]
+    public async Task<IActionResult> GetLocations()
+    {
+        var response = await _client.GetAsync("/Locations");
+        if (!response.IsSuccessStatusCode) return StatusCode((int)response.StatusCode);
+        return Ok(await response.Content.ReadAsStringAsync());
+    }
 }
 
